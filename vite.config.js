f4bb697646c8be5a47bc551e33dcde75bb06ffbe
st-tanngo <tan.ngo@supremetech.vn>
2025-05-30 18:57:08 +0700

    Finish FE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 8088,
+    allowedHosts: ['capstone2projectfe-production.up.railway.app']
+  }
 })
